70f5dfec4d3ee1718f155aa649d07c4137e2ef3f
Am1d1v <124504237+Am1d1v@users.noreply.github.com>
2023-10-05 18:32:17 +0300

    Add files via upload

--- localStorage.js
+++ localStorage.js
@@ -13,6 +13,29 @@ checkbox.addEventListener('change', () => {
     localStorage.setItem('isChecked', true);
 });
 
+if(localStorage.getItem('bg') === 'changed'){
+    form.style.backgroundColor = "pink";
+}
 
+
+change.addEventListener('click', () => {
+    if(localStorage.getItem('bg') === 'changed'){
+        localStorage.removeItem('bg');
+        form.style.backgroundColor = "white";
+    } else {
+        localStorage.setItem('bg', 'changed');
+        form.style.backgroundColor = "gray";
+    }
+})
+
+
+const person = {
+    name: 'Name',
+    age: 30
+}
+
+const serializedPerson = JSON.stringify(person);
+localStorage.setItem('person', serializedPerson);
+console.log(localStorage.getItem('person'));
 
 
